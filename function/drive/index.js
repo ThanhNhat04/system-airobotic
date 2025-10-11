b25a899825705fdb2c20d7435ad56b9057af8499
Vamnaone <vamnaone@gmail.com>
2025-10-11 15:39:50 +0700

    d

--- function/drive/index.js
+++ function/drive/index.js
@@ -1,13 +1,13 @@
 import { google } from 'googleapis';
 
 export async function getDriveClient() {
-    const auth = new google.auth.GoogleAuth({
-        projectId: process.env.GOOGLE_PROJECT_ID,
-        credentials: {
-            client_email: process.env.GOOGLE_CLIENT_EMAIL,
-            private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-        },
-        scopes: ['https://www.googleapis.com/auth/drive'],
-    });
-    return google.drive({ version: 'v3', auth });
+  const auth = new google.auth.GoogleAuth({
+    projectId: process.env.GOOGLE_PROJECT_ID,
+    credentials: {
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    },
+    scopes: ['https://www.googleapis.com/auth/drive'],
+  });
+  return google.drive({ version: 'v3', auth });
 }
